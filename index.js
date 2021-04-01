6861e6cd8d8d41e900182143673b5669b874dbfb
nurulabsar-git <mohammad.nurulabsar20203025@yahoo.com>
2021-04-02 01:55:31 +0600

    update

--- index.js
+++ index.js
@@ -10,16 +10,16 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cors());
 
-console.log(process.env.DB_USER);
+// console.log(process.env.DB_USER);
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ivwbd.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
- console.log('error test', err);
+//  console.log('error test', err);
   const assignmentCollection = client.db("assignment").collection("grocery");
-  console.log('Connection successfully')
+  // console.log('Connection successfully')
  
 
   app.post('/addGrocery', (req, res) => {
@@ -28,7 +28,7 @@ client.connect(err => {
     console.log("new data: ", newData);
     assignmentCollection.insertOne(newData)
     .then(result => {
-      console.log('inserted count', result.insertedCount)
+      // console.log('inserted count', result.insertedCount)
       res.send(result.insertedCount > 0)
     })
 
@@ -38,10 +38,22 @@ client.connect(err => {
   app.get('/products', (req, res) => {
     assignmentCollection.find()
     .toArray((err, products) => {
-      console.log("from database", products)
+      // console.log("from database", products)
       res.send(products)
     })
   })
+
+
+  app.delete('/deleteProduct/:id', (req, res) =>{
+
+    const id = ObjectID(req.params.id);
+    console.log('delete text', id);
+    assignmentCollection.deleteOne({_id: id})
+    .then((err, documents) => res.send({
+      success: true
+    }))
+  })
+  
 
 
 });
